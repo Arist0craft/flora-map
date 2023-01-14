e5247632f78992d69f0b69eae464d8e3d0a9c5b1
Arist0craft <dimnktn@gmail.com>
2023-01-15 01:57:12 +0300

    Интегрирован Webpack

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+const path = require('path');
+
+/** @type {import('webpack').Configuration} */
+module.exports = {
+  "entry": path.resolve(__dirname, "src"),
+  "output": {
+    "filename": "map.js",
+    "path": path.resolve(__dirname, "dist/js"),
+  }
+};
