9dcdf7ca258a3e85fb5ecebcd1efc571956d4b08
Arist0craft <dimnktn@gmail.com>
2023-01-15 01:19:34 +0300

    Создание шаблона поиска Добавлено: расширение шаблона поиска, новые прослушки событий Изменено: наименования классов

--- js/bundle.js
+++ js/bundle.js
@@ -3,7 +3,7 @@
 // Реализует интерфейс IGeocodeProvider.
 
 /** Кастомный класс геокодера */
-class CustomSearchProvider {
+class SearchProvider {
   constructor(geoPoints) {
     // Геоколлекция, по которой происходит поиск
     this.geoPoints = geoPoints;
@@ -108,22 +108,25 @@ function getData() {
       pochta: "info@floratrack.ru",
       phone: "8(800)555-35-35",
       wh: "8:00-19:00",
+      type: "warehouse"
     },
     {
       coords: [55.906239, 37.430992],
-      name: "Склад (Химки)",
+      name: "Склад Химки",
       address: "МО, г. Химки, Заводская улица, вл11",
       pochta: "info@floratrack.ru",
       phone: "8(800)",
       wh: "00:00-24:00",
+      type: "city"
     },
     {
       coords: [55.107118, 83.05268],
-      name: "Склад (Новосибирский)",
+      name: "Склад Новосибирский",
       address: "Новосибирский район, с. Каменка, ул. Заводская д.28а стр 9",
       pochta: "info@floratrack.ru",
       phone: "8(800)",
       wh: "00:00-24:00",
+      type: "soon"
     },
   ];
   return data;
@@ -165,27 +168,106 @@ function getMapMarkers(geoPoints) {
 
 
 
-function buildSearhFormLayout() {
-  const MySearchFormLayout = ymaps.templateLayoutFactory.createClass(
+/**
+ * Создание шаблона класса поисковой формы для Яндекс.Карт
+ * @returns Шаблон поисковой формы
+ */
+function buildSearhLayout() {
+  const domEventManager = ymaps.domEvent.manager;
+
+  // Задаём шаблон
+  const SearchLayout = ymaps.templateLayoutFactory.createClass(
     `
-    <div class="map-search">
-      <button>
+    <div class="map-search" id="map-search">
+      <button id="map-search-button">
         <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path fill-rule="evenodd" clip-rule="evenodd" d="M10.7579 0.333252C9.12719 0.333387 7.52015 0.711677 6.07085 1.43656C4.62156 2.16145 3.37205 3.21191 2.42656 4.5003C1.48108 5.78869 0.86704 7.27766 0.635678 8.84297C0.404315 10.4083 0.562336 12.0046 1.09656 13.4986C1.63078 14.9926 2.52571 16.3411 3.70669 17.4316C4.88766 18.522 6.32044 19.3228 7.88547 19.7671C9.45051 20.2114 11.1024 20.2863 12.7034 19.9857C14.3044 19.685 15.808 19.0174 17.0888 18.0386L21.4826 22.2992C21.7095 22.5118 22.0134 22.6294 22.3289 22.6267C22.6443 22.624 22.9461 22.5013 23.1691 22.285C23.3922 22.0687 23.5187 21.7761 23.5215 21.4702C23.5242 21.1643 23.403 20.8696 23.1838 20.6496L18.79 16.3889C19.9787 14.9266 20.7188 13.1694 20.9256 11.3183C21.1325 9.46714 20.7978 7.59694 19.9597 5.92169C19.1216 4.24645 17.8141 2.83383 16.1868 1.84552C14.5595 0.857209 12.6781 0.333127 10.7579 0.333252ZM2.93761 10.2499C2.93761 8.23869 3.76153 6.30984 5.22812 4.88769C6.69472 3.46554 8.68384 2.66659 10.7579 2.66659C12.832 2.66659 14.8211 3.46554 16.2877 4.88769C17.7543 6.30984 18.5782 8.23869 18.5782 10.2499C18.5782 12.2611 17.7543 14.19 16.2877 15.6121C14.8211 17.0343 12.832 17.8333 10.7579 17.8333C8.68384 17.8333 6.69472 17.0343 5.22812 15.6121C3.76153 14.19 2.93761 12.2611 2.93761 10.2499Z" fill="#242424"/>
         </svg>
       </button>
-      <input id="map-suggest" type="search" placeholder="{{ options.placeholderContent }}">
+      <input id="map-search-input" type="search" placeholder="{{ options.placeholderContent }}">
     </div>
-    {% include options.popupLayout %}
     `,
     {
       build: function () {
-        this._elements = { test: "test" };
+        /*
+         * Замена конструктора класса шаблона
+         */
+        SearchLayout.superclass.build.call(this);
+        this._elements = {
+          input: this._getElement("map-search-input"),
+          button: this._getElement("map-search-button"),
+        };
+        // Добавляем прослушивание событий на наши элементы
+        this._createElementListeners();
+        // Добавляем поисковые подсказки к нашему полю ввода
+        this._createSuggestPanel();
+        // const data = this.getData(),
+        //       state = data.state,
+        //       sharedState = data.sharedState;
+
+        // Добавляем элемент поля ввода в расшаренное состояние
+        // Это позволит забирать поисковый запрос на событии formsubmit
+        sharedState.set({formInput: this._elements.input});
+
       },
+      _getElement: function(id) {
+        return document.getElementById(id);
+      },
+
+      _createElementListeners: function() {
+        // Добавление прослушивания событий на элементы формы
+        const e = this._elements;
+        this._elementListeners = {
+          input: domEventManager.group(e.input),
+          button: domEventManager.group(e.button),
+        }
+        this._elementListeners.input
+          .add("keypress", this._onInputKeyPress, this)
+          .add("keyup", this._onInputKeyUp, this);
+        this._elementListeners.button.add(
+          "click", 
+          this._onButtonClick, this
+          );
+      },
+      _onInputKeyPress: function(domEvent) {
+        /*
+        / Проверка, если вводимый символ клавиша Enter,
+        / т.е отправка поискового запроса
+        */
+        if ("13" == domEvent.get("charCode") || "13" == domEvent.get("keyCode"))
+          return (
+            this._formSubmit(),
+            (domEvent.originalEvent.cancelBubble = !0),
+            (domEvent.originalEvent.returnValue = !1),
+            domEvent.originalEvent.preventDefault &&
+              domEvent.originalEvent.preventDefault(),
+            !1
+          );
+      },
+      _onInputKeyUp: function(domEvent) {
+        /*
+         * Помещаем в состояние значение поля ввода
+         */
+        this.getData().state.set("inputValue", this._elements.input.value);
+      },
+
+      _onButtonClick: function(domEvent) {
+        // Клик на кнопку поискового запроса инициирует поиск
+        this._formSubmit();
+      },
+      
+      _formSubmit: function () {
+        // Отправка поискового запроса
+        this.events.fire("formsubmit");
+      },
+
+      _createSuggestPanel: function() {
+
+      }
     }
   );
 
-  return MySearchFormLayout;
+  return SearchLayout;
 }
 
 /**
@@ -195,18 +277,18 @@ function buildSearhFormLayout() {
  */
 function getMySearchControl(geoPoints) {
   // Меняем форму объектов поисковой выдачи
-  const MySearchControlPopupItemLayoutClass =
-    ymaps.templateLayoutFactory.createClass(
-      `
-    <div class="item_container">
-      <div class="item_name">{{ data.name }}</div>
-      <div class="item_address">{{ data.address }}</div>
-      <div class="item_address">{{ data.pochta }}</div>
-      <div class="item_phone">{{ data.phone|default:"неизвестно" }}</div>
-      <div class="item_wh">Время работы: {% if data.wh %} {{ data.wh }} {% else %} не работает{% endif %}</div>
-    </div>
-    `
-    );
+  // const MySearchControlPopupItemLayoutClass =
+  //   ymaps.templateLayoutFactory.createClass(
+  //     `
+  //   <div class="item_container">
+  //     <div class="item_name">{{ data.name }}</div>
+  //     <div class="item_address">{{ data.address }}</div>
+  //     <div class="item_address">{{ data.pochta }}</div>
+  //     <div class="item_phone">{{ data.phone|default:"неизвестно" }}</div>
+  //     <div class="item_wh">Время работы: {% if data.wh %} {{ data.wh }} {% else %} не работает{% endif %}</div>
+  //   </div>
+  //   `
+  //   );
 
 
 
@@ -214,15 +296,15 @@ function getMySearchControl(geoPoints) {
   const mySearchControl = new ymaps.control.SearchControl({
     options: {
       // Заменяем стандартный провайдер данных (геокодер) нашим собственным.
-      provider: new CustomSearchProvider(geoPoints),
+      provider: new SearchProvider(geoPoints),
       // Не будем показывать еще одну метку при выборе результата поиска,
       // т.к. метки коллекции myCollection уже добавлены на карту.
       noPlacemark: true,
       noCentering: true,
       resultsPerPage: 5,
       placeholderContent: "Поиск склада",
-      popupItemLayout: MySearchControlPopupItemLayoutClass,
-      // normalLayout: buildSearhFormLayout(),
+      // popupItemLayout: MySearchControlPopupItemLayoutClass,
+      // layout: buildSearhLayout(),
     },
   });
 
