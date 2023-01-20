e33616428f0057b2e990c9ec726c10a3ff85ff34
Arist0craft <dimnktn@gmail.com>
2023-01-20 12:35:18 +0300

    Изменён скролл карты Добавлено: контроллер, определяющий поведение карты при скролле: необходимо кликнуть или тапнуть на карту, чтобы она стала интерактивной

--- src/index.js
+++ src/index.js
@@ -1,11 +1,105 @@
 import getMySearchControl from "./SearchControl"
 import { ButtonController, addMapControls } from "./ButtonController"
 
+
+/**
+ * Включение и отключение автоматического скролла для улучшения навигации по
+ * странице
+ * @param {DOMElement} mapSection Контейнер карты и её управления
+ * @param {ymaps.Map} map Инстанс карты
+ */
+function controlMapMobileScrolling(mapSection, map) {
+  // Отключаем скролл карты по дефолту
+  map.behaviors.disable(["scrollZoom", "drag"]);
+
+  /**
+   * Включение зума на скролл при клике на карту
+   * @param {Event} e Событие клика на карту
+   */
+  function onClickMapScrollEnable(e){
+    if (!map.behaviors.isEnabled("scrollZoom")) {
+      map.behaviors.enable("scrollZoom");
+
+    }
+  }
+
+  /**
+   * Отключение зума на скролл при клике на любой элемент, кроме карты
+   * @param {Event} e Событие клика вне карты
+   */
+  function onClickMapScrollDisable(e) {
+    if (
+      !mapSection.contains(e.target) 
+      && map.behaviors.isEnabled("scrollZoom")
+      ) {
+      map.behaviors.disable("scrollZoom");
+    }
+  }
+
+  /**
+   * При таче на карту / элементы управления включаем драг карты
+   * @param {Event} e Событие на начало тача
+   */
+  function onTouchStartMap(e) {
+    map.isTouchMoved = false;
+  }
+
+  /**
+   * Если тач карты был с движением, пример скролл страницы, то ставим флаг
+   * @param {Event} e Событие движения при таче
+   */
+  function onTouchMoveMap(e) {
+    map.isTouchMoved = true;
+  }
+
+  /**
+   * Если тач карты был с движением, то не включаем движение карты, или включаем драг
+   * @param {Event} e Событие движения при таче
+   */
+  function onTouchEndMapDragEnable(e) {
+    if (
+      !map.isTouchMoved
+      && !map.behaviors.isEnabled("drag")
+      ) {
+      map.behaviors.enable("drag");
+    }
+  }
+  
+  /**
+   * Если тач был вне карты или кнопок фильтрации, то отключаем драг
+   * @param {Event} e Событие тача вне карты
+   */
+  function onTouchEndMapDragDisable(e) {
+    if (
+      ![...mapSection.querySelectorAll(".control"), mapSection.children.map]
+        .some(c => c.contains(e.target))
+      && map.behaviors.isEnabled("drag")
+    ) {
+      map.behaviors.disable("drag");
+    }
+  }
+
+  for (let e of mapSection.children) {
+    e.addEventListener("click", onClickMapScrollEnable);
+  }
+
+  mapSection.children.map.addEventListener("touchstart", onTouchStartMap);
+  mapSection.children.map.addEventListener("touchmove", onTouchMoveMap);
+  mapSection.children.map.addEventListener("touchend", onTouchEndMapDragEnable);
+
+  // Отключаем скролл зум, если клик произошёл вне карты
+  document.body.addEventListener("click", onClickMapScrollDisable);
+  
+  // Если клик или тач произошёл вне карты, то отключаем зум
+  document.body.addEventListener("touchend", onTouchEndMapDragDisable);
+} 
+
+
 /**
  * Инициализация Яндекс.Карт
  */
 function initYandexMapsCallback() {
-
+  const mapSection = document.querySelector(".section-map");
 
   // Создаём объект карты
   const myMap = new ymaps.Map("map", {
@@ -13,7 +107,6 @@ function initYandexMapsCallback() {
     zoom: 5,
     controls: [],
   });
-
 
   // Задаём управление фильтрами
   const buttonController = new ButtonController(myMap);
@@ -34,6 +127,8 @@ function initYandexMapsCallback() {
     }
   }));
 
+  // Контроль скролла карты
+  controlMapMobileScrolling(mapSection, myMap);
 }
 
 
